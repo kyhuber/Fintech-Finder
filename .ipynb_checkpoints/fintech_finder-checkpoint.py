d941890123224059fcd23049c8d1f54a657b3eea
unknown <kyhuber@gmail.com>
2022-05-11 13:26:19 -0700

    Update send transaction code

--- .ipynb_checkpoints/fintech_finder-checkpoint.py
+++ .ipynb_checkpoints/fintech_finder-checkpoint.py
@@ -271,7 +271,8 @@ if st.sidebar.button("Send Transaction"):
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
     send_transaction(w3, account, candidate_address, wage)
-    
+    TX = transaction_hash
+
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
 
